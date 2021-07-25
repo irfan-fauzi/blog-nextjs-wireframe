fffbb65c211c114fd68b87dee3e9367093e8608c
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-25 21:32:23 +0700

    tumbnail

--- components/GridContents.js
+++ components/GridContents.js
@@ -3,8 +3,52 @@ const GridContents = () => {
 
   const content = [
     {
-      img: ''
+      img: 'https://raw.githubusercontent.com/irfan-fauzi/blog-nextjs-wireframe/main/assets/thumbnail-2.jpg',
+      categorie: 'internet',
+      date: '18 juli 2021',
+      title: 'how to design your product that can grow 20x',
+      content : 'Auctor Porta. Augue vitae diam mauris faucibus blandit elit per, feugiat leo dui orci. Etiam vestibulum. Nostra netus per conubia dolor.',
+      imgWriter: '',
+      nameWriter: '',
+      profesion: ''
+
+    },
+    {
+      img: 'https://raw.githubusercontent.com/irfan-fauzi/blog-nextjs-wireframe/main/assets/thumbnail-3.jpg',
+      categorie: 'inspiration',
+      date: '18 juli 2021',
+      title: 'the more important the work, the more important the rest',
+      content : 'Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.'
+    },
+    {
+      img: 'https://raw.githubusercontent.com/irfan-fauzi/blog-nextjs-wireframe/main/assets/thumbnail-4.jpg',
+      categorie: 'inspiration',
+      date: '18 juli 2021',
+      title: 'email Love- email inspiration template and discovery',
+      content : 'Consider that for a moment: everything we see around us is assumed to have had a cause and is contingent upon something else.'
+    },
+    {
+      img: 'https://raw.githubusercontent.com/irfan-fauzi/blog-nextjs-wireframe/main/assets/thumbnail-5.jpg',
+      categorie: 'inspiration',
+      date: '18 juli 2021',
+      title: 'The More Important the Work, the More Important the Rest',
+      content : 'Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.'
+    },
+    {
+      img: 'https://raw.githubusercontent.com/irfan-fauzi/blog-nextjs-wireframe/main/assets/thumbnail-6.jpg',
+      categorie: 'inspiration',
+      date: '18 juli 2021',
+      title: 'Email Love - Email Inspiration, Templates and Discovery',
+      content : 'Consider that for a moment: everything we see around us is assumed to have had a cause and is contingent upon something else.'
+    },
+    {
+      img: 'https://raw.githubusercontent.com/irfan-fauzi/blog-nextjs-wireframe/main/assets/thumbnail-7.jpg',
+      categorie: 'inspiration',
+      date: '18 juli 2021',
+      title: 'How to design a product that can grow itself 10x in year',
+      content : 'Auctor Porta. Augue vitae diam mauris faucibus blandit elit per, feugiat leo dui orci. Etiam vestibulum. Nostra netus per conubia dolor.'
     }
+    
   ]
   return (
     <div>
